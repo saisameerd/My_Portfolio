35f588803e6c0781523f58af539a74686380e38b
saisameerd <saisameer.d@gmail.com>
2022-01-18 19:58:54 +0530

    add image effect and responsive changes

--- js/script.js
+++ js/script.js
@@ -5,9 +5,17 @@ window.onmousemove = (e) =>{
     cursor.style.left = e.pageX + 'px';
 }
 
-let navbar = document.querySelector('.navbar');
+let home_img = document.querySelector('.home .home-img');
+let home = document.querySelector('.home');
+
+home.onmousemove = (e) =>{
+    home_img.style.top = e.pageY + 'px';
+    home_img.style.left = e.pageX + 'px';
+}
 
 let menu_btn = document.querySelector('#menu-btn');
+
+let navbar = document.querySelector('.navbar');
 
 menu_btn.onclick = () =>{
     navbar.classList.toggle('active');
