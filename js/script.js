f8edb9e558b62e312343ce3a83658b3d821f766f
saisameerd <saisameer.d@gmail.com>
2022-01-18 13:59:39 +0530

    added navbar and menu-btn

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ window.onmousemove = (e) =>{
     cursor.style.top = e.pageY + 'px';
     cursor.style.left = e.pageX + 'px';
 }
+
+let navbar = document.querySelector('.navbar');
+
+// let menu_btn = document.querySelector('#menu-btn');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
